2331d166296c853bf76b995b48c1669db6e1d561
d-stafford <d-stafford@SA210-A6.ad.onu.edu>
2015-03-24 09:02:52 -0400

    Initial Commit

---
+++ src/mp4/MP4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mp4;
+
+/**
+ *
+ * @author d-stafford
+ */
+public class MP4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
